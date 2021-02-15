32ac56855e04be3795d66031d5f910082e75cd2b
Will <will_grc@hotmail.com>
2021-02-15 13:06:56 -0500

    Ajuste para traer la fecha de inicio en liquidaciones de prestaciones

--- movimientos/clases/CesantiasModelClass.php
+++ movimientos/clases/CesantiasModelClass.php
@@ -651,7 +651,7 @@ final class CesantiasModel extends Db{
 		$select = "SELECT c.contrato_id,
 		SUM(c.sueldo_base+subsidio_transporte) AS sueldo_base,
 		(SELECT MAX(fecha_corte) FROM liquidacion_cesantias WHERE contrato_id=c.contrato_id AND estado='C' ) AS fecha_ultimo_corte,
-		c.fecha_inicio,
+		IF(c.fecha_ult_cesantias IS NOT NULL,c.fecha_ult_cesantias,c.fecha_inicio)AS fecha_inicio,
 		(SELECT nombre_cargo FROM cargo WHERE cargo_id=c.cargo_id) AS cargo,
 		(SELECT t.numero_identificacion FROM tercero t, empleado e WHERE t.tercero_id=e.tercero_id AND e.empleado_id=c.empleado_id) AS numero_identificacion,
 		(SELECT CONCAT_WS(' ', t.primer_nombre,t.segundo_nombre,t.primer_apellido,t.segundo_apellido) FROM tercero t, empleado e WHERE t.tercero_id=e.tercero_id AND e.empleado_id=c.empleado_id) AS empleado
